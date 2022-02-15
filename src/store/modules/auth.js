71ebf9968910d4aa6c89b1802af8a4ffc035e6c5
Дом <dom@MacBook-Air-Dom.local>
2022-02-15 23:18:44 +0300

    add api

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,21 +1,44 @@
+import authApi from "@/api/auth";
+
 const state = {
-    isSubmiting: false
-}
+  isSubmiting: false,
+};
 
 const mutations = {
-    registerStart(state) {
-        state.isSubmiting = true
-    }
-}
+  registerStart(state) {
+    state.isSubmiting = true;
+  },
+
+  registerSucces(state) {
+    state.isSubmiting = false;
+  },
+
+  registerFailure(state) {
+    state.isSubmiting = false;
+  },
+};
 
 const actions = {
-    register(context) {
-
-    }
-}
+  register(context, credentials) {
+      console.log(1)
+    return new Promise((resolve) => {
+      context.commit("registerStart");
+      authApi
+        .register(credentials)
+        .then((response) => {
+          context.commit("registerSucces", response.data.user);
+          resolve(response.data.user);
+        })
+        .catch((result) => {
+          context.commit("registerFailure", result.response.data.errors);
+          console.log(result.response.data);
+        });
+    });
+  },
+};
 
 export default {
-    state,
-    mutations,
-    actions
-}
+  state,
+  mutations,
+  actions,
+};
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,21 +5,38 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign Up</h1>
           <p class="text-xs-center">
-            <router-link :to="{name: 'home'}">Need an account?</router-link>
+            <router-link :to="{ name: 'home' }">Need an account?</router-link>
           </p>
           VALIDATION ERRORS WIP
           <form @submit.prevent="onSubmit">
             <fieldset class="form-group">
-              <input type="text" class="form-control form-control-lg" placeholder="Username">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Username"
+              />
             </fieldset>
             <fieldset class="form-group">
-              <input type="text" class="form-control form-control-lg" placeholder="Email">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Email"
+              />
             </fieldset>
 
             <fieldset class="form-group">
-              <input type="password" class="form-control form-control-lg" placeholder="Password">
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="Password"
+              />
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right" :disabled="isSubmiting">Sign Up</button>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmiting"
+            >
+              Sign Up
+            </button>
           </form>
         </div>
       </div>
@@ -29,16 +46,20 @@
 
 <script>
 export default {
-  name: 'McvRegister',
+  name: "McvRegister",
   computed: {
     isSubmiting() {
-      return this.$store.state.auth.isSubmiting
-    }
+      return this.$store.state.auth.isSubmiting;
+    },
   },
   methods: {
     onSubmit() {
-      this.$store.commit('registerStart')
-    }
-  }
-}
+      this.$store.dispatch("register",  {
+        email: "sdgsdnnhjgsd@mai.ru",
+        username: "sdgsgsdghfgjh",
+        password: "dagnl4n6l34nlk"
+      }).then(user => {console.log(user)});
+    },
+  },
+};
 </script>
