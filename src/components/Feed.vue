89e61c0c492530d091efaf392244efeb3dea06e0
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-02-23 21:01:37 +0300

    add computed offset

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -46,10 +46,10 @@
         </router-link>
       </div>
       <mcv-pagination
-        :total="total"
+        :total="feed.articlesCount"
         :limit="limit"
         :current-page="currentPage"
-        :url="url"
+        :url="baseUrl"
       />
     </div>
   </div>
@@ -59,6 +59,8 @@
 import { mapState } from "vuex";
 import { actionTypes } from "@/store/modules/feed";
 import McvPagination from "@/components/Pagination";
+import { limit } from "@/helpers/vars";
+import { stringify, parseUrl } from "query-string";
 
 export default {
   name: "McvFeed",
@@ -73,10 +75,7 @@ export default {
   },
   data() {
     return {
-      total: 500,
-      limit: 10,
-      currentPage: 5,
-      url: "/tags/dragons",
+      limit,     
     };
   },
   computed: {
@@ -85,9 +84,35 @@ export default {
       feed: (state) => state.feed.data,
       error: (state) => state.feed.error,
     }),
+    currentPage() {
+      return Number(this.$route.query.page || '1');
+    },
+    baseUrl() {
+      return this.$route.path;
+    },
+    offset() {
+      return this.currentPage * limit - limit
+    }
+  },
+  watch: {
+    currentPage() {
+       this.fetchFeed();
+    }
   },
   mounted() {
-    this.$store.dispatch(actionTypes.getFeed, { apiUrl: this.apiUrl });
+    this.fetchFeed();
   },
+  methods: {
+    fetchFeed() {
+      const parsedUrl = parseUrl(this.apiUrl);
+      const stringifiedParams = stringify({
+        limit,
+        offset: 0,
+        ...parsedUrl.query
+      })
+      const apiUrlWitchParams = `${parsedUrl.url}?${stringifiedParams}`
+      this.$store.dispatch(actionTypes.getFeed, { apiUrl: apiUrlWitchParams });
+    }
+  }
 };
 </script>
---
+++ src/helpers/vars.js
@@ -0,0 +1,3 @@
+export const limit = 10;
+
+
